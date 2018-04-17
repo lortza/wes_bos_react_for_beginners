c06a90b7c35f36e8f2a1ddb2aaf4438c7f43b31b
anne <richardson.ae@gmail.com>
2018-04-17 16:16:08 -0500

    Adds state to App and functions to update it

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,9 +3,31 @@ import React from "react";
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
 
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+
+  addFish = (fish) => {
+    // 1. Make a copy of the existing state
+    const newFishes = {...this.state.fishes}
+    // 2. Add new fish to fishes const
+    newFishes[`fish${Date.now()}`] = fish
+    // 3. Set state to include new fishes
+    this.setState({
+      fishes: newFishes
+    })
+  }
+
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render(){
     return (
       <div className="catch-of-the-day">
@@ -13,7 +35,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
           <Order />
-          <Inventory />
+          <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
   }
